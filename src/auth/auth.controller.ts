00caeded8ee337bc3fc5d244f50d9f329c7b7e44
LewisShim <lewis@atomrigs.io>
2022-12-12 23:24:49 +0900

    init

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Request } from 'express'
+import { AuthDto } from './dto';
+
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup(/*@Req() req: Request*/
+    @Body() dto: AuthDto) {
+        return this.authService.signup(dto)
+    }
+
+    @Post('signin')
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signin(dto)
+    }
+}
